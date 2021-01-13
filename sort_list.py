ad7ed9adf9842c18b9f56606afd0a42f981fc28c
Tobias Llop <65455969+tobiasllop@users.noreply.github.com>
2021-01-13 11:50:41 -0300

    Add files via upload

---
+++ sort_list.py
@@ -0,0 +1,6 @@
+dayly_challenges = ["Design an Outfit for Ellie", "Light in your Kitchen", "Use the Twilio API", "Build a Data Visualization", "Write Code to Sort a List", "Submit a Challenge with Someone in your Guild", "Make a Logo for a Project", "Explore Hardware (Emulators Welcome!)", "Build a Battlesnake", "Share a Meal with Someone in Your Guild"]
+
+print(f"First list: \n {dayly_challenges} \n")
+
+dayly_challenges.sort(reverse = True)
+print(f"Sorted list: \n {dayly_challenges} \n") 
